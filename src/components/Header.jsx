f75331cfae6cf22cb1cf6900f83ff673a2187914
AdityaMore7000 <adi.more7000@gmail.com>
2023-05-07 16:39:11 +0530

    added img directory

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import Logo from '../../public/img/ri_instagram-fill.png'
 const Header = ()=>{
     return(
         <>
+        <img src={Logo} alt="Logo" />
             <h1 className="text-9xl text-red-600 font-extrabold bg-amber-200 text-center">This is the header</h1>
         </>
         )
